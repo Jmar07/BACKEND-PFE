9c14305b2cd6ab08597206895a83f9c2a6911c3d
chouikh <chm3sa@gmail.com>
2022-05-01 17:09:06 +0100

    last changes

--- index.js
+++ index.js
@@ -3,11 +3,24 @@ require("./db")
 const app = express();
 const routes = require("./routes")
 const bodyParser =require("body-parser")
-const cors=require("cors");
-
 const cookieParser = require("cookie-parser");
 const sessions = require('express-session');
 
+var cors = require('cors')
+
+
+var corsOptions = {
+  origin: 'http://localhost:4200',
+  optionsSuccessStatus: 202
+}
+
+app.use(cors())
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use(sessions({
     secret: 'hunter',
@@ -16,12 +29,6 @@ app.use(sessions({
   }));
 
 app.use(cookieParser());
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
 
 app.use(express.json())
 
